5d19743be3c787e71d4e75962a3ca79b587a9525
Cody Rayburn <rayburn.cody12@gmail.com>
2020-02-20 15:23:08 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+const Typewriter = function(txtElement, words, wait = 3000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = '';
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+}
+
+// Type Method
+Typewriter.prototype.type = function() {
+    // Current index of word
+    const current = this.wordIndex % this.words.length;
+    // Full text of current word
+    const fullTxt = this.words[current];
+    // Check if deleting state
+    if(this.isDeleting) {
+        // Remove char
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        // Add char
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    // Insert txt into element
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+    // Initial Type Speed
+    let typeSpeed = 300;
+
+    if(this.isDeleting) {
+        typeSpeed /= 2;
+    }
+
+    // Check for complete word
+    if(!this.isDeleting && this.txt === fullTxt) {
+        typeSpeed = this.wait;
+        this.isDeleting = true;
+    } else if(this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.wordIndex++;
+        typeSpeed = 500;
+    }
+
+    setTimeout(() => this.type(), typeSpeed)
+}
+
+// Init on DOM load
+document.addEventListener('DOMContentLoaded', init);
+// Init app
+function init() {
+    const txtElement = document.querySelector('.txt-type');
+    const words = JSON.parse(txtElement.getAttribute('data-words'));
+    const wait = txtElement.getAttribute('data-wait');
+    // Init typewriter
+    new Typewriter(txtElement, words, wait);
+}
